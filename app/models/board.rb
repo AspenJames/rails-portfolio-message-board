6fe34ea74fb96a210fc626bc1aad374a064013fd
AspenJames <aspenjames@tqca.org>
2018-06-19 16:33:21 -0700

    add associations to models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,9 @@
 class Board < ApplicationRecord
+  has_many :messages
+  has_many :users, :through => :messages
+
+  def creator
+    @creator ||= User.find(self.created_by)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :boards, :through => :messages
+  has_many :messages
+
+  def created_boards
+    @created = []
+    @created << Board.where(:created_at => self.id)
+  end
+
 end
