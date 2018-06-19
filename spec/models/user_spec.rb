e665ff15072afee8ef11ac7e08ffd9fa1afa058f
AspenJames <aspenjames@tqca.org>
2018-06-19 16:46:33 -0700

    add User creation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create(
+      :name => "Aspen James"
+      :username => "ajames"
+      :email => "aspen@email.co"
+      :password => "password"
+    )
+  }
+
+  it "is valid with a name, username, email, and password" do
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a password" do
+    expect(User.new(:username => "PwrdHater", :name => "John Doe", :email => "jdoe@hacked.co")).not_to be_valid
+  end
+
+  it "is not valid without a username" do
+    expect(User.new(:name => "John Doe", :email => "jdoe@hacked.co", :password => "password")).not_to be_valid
+  end
+
+  it "is not valid without an email" do
+    expect(User.new(:username => "PwrdHater", :name => "John Doe", :password => "password")).not_to be_valid
+  end
+
 end
