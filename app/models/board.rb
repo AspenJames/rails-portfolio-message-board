8d55fe08c985d61a4d41f5ff9b27fe081748d092
AspenJames <aspenjames@tqca.org>
2018-06-19 17:29:09 -0700

    add validations to Board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,8 @@
 class Board < ApplicationRecord
   has_many :messages
   has_many :users, :through => :messages
+  validates :topic, :presence => true
+  validates :created_by, :presence => true
 
   def creator
     @creator ||= User.find(self.created_by)
