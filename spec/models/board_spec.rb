6195e2387df5831fdce720810c7c8de338d86572
AspenJames <aspenjames@tqca.org>
2018-06-19 17:17:00 -0700

    add Board creation spec

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create(
+      :name => "Aspen James",
+      :username => "ajames",
+      :email => "aspen@email.co",
+      :password => "password"
+    )
+  }
+
+  let(:board) {
+    Board.create(
+      :topic => "Coffee Break",
+      :created_by => user.id
+    )
+  }
+
+  let(:message_one) {
+    Message.create(
+      :content => "First message",
+      :board_id => board.id,
+      :user_id => user.id
+    )
+  }
+
+  let(:message_two) {
+    Message.create(
+      :content => "Second message",
+      :board_id => board.id,
+      :user_id => user.id
+    )
+  }
+
+  it "is valid with a topic and created_by" do
+    expect(board).to be_valid
+  end
+
+  it "is invalid without a topic" do
+    expect(Board.new(:created_by => user.id)).not_to be_valid
+  end
+
+  it "is invalid without a created_by" do
+    expect(Board.new(:topic => "Invalid Data Protections")).not_to be_valid
+  end
+
 end
