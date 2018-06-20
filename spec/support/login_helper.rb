3391fe2f6329d875deeeb0b6512a6aa88c6a83d5
AspenJames <aspenjames@tqca.org>
2018-06-20 15:21:54 -0700

    add user_signup, create_user, and user_login helper methods

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,7 +1,26 @@
 module LoginHelper
 
   def user_signup
-    
+    fill_in("user[name]", :with => "Aspen James")
+    fill_in("user[username]", :with => "ajames")
+    fill_in("user[email]", :with => "aspen@email.co")
+    fill_in("user[password]", :with => "password")
+    click_button('Sign Up')
+  end
+
+  def create_user
+    @jdoe = User.create(
+      :name => "John Doe",
+      :username => "jdoe",
+      :email => "jdoe@anon.com",
+      :password => "password"
+    )
+  end
+
+  def user_login
+    fill_in("user[username]", :with => "jdoe")
+    fill_in("user[password]", :with => "password")
+    click_button("Log In")
   end
 
 end
