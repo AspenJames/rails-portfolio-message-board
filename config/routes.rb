f0b6bc58c5ed69e4c159d72ebbe2ea55351f0758
AspenJames <aspenjames@tqca.org>
2018-06-20 15:29:07 -0700

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'boards#index'
+
   resources :users, :only => [:index, :show, :edit, :update, :destory]
   get '/signup' => 'users#new', :as => "signup"
   post '/signup' => 'users#create'
