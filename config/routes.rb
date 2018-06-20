0cec0b6d338b0ed304b80d3acd0cbc57cb930da4
AspenJames <aspenjames@tqca.org>
2018-06-20 15:26:46 -0700

    create custom routes for signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users, :only => [:index, :show, :edit, :update, :destory]
+  get '/signup' => 'users#new', :as => "signup"
+  post '/signup' => 'users#create'
 
   resources :boards do
     resources :messages
   end
 
-  resources :sessions, :only => [:new, :create]
+  get '/login' => 'sessions#new', :as => "login"
+  post '/login' => 'sessions#create'
 
 end
