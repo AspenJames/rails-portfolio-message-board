45e2b9cc74a8717dbda5002d8c90990eda600541
AspenJames <aspenjames@tqca.org>
2018-06-19 17:11:12 -0700

    add Message creation specs

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create(
+      :name => "Aspen James",
+      :username => "ajames",
+      :email => "aspen@email.co",
+      :password => "password"
+    )
+  }
+
+  let(:board) {
+    Board.create(
+      :topic => "Coffee Break",
+      :created_by => user.id
+    )
+  }
+
+  let(:message) {
+    Message.create(
+      :content => "This is a valid message!",
+      :user_id => user.id,
+      :board_id => board.id
+    )
+  }
+
+  it "is valid with content, user_id, and board_id" do
+    expect(message).to be_valid
+  end
+
+  it "is not valid without a user_id" do
+    expect(Message.new(:content => "This message is invalid", :board_id => board.id)).not_to be_valid
+  end
+
+  it "is not valid without a board_id" do
+    expect(Message.new(:content => "This message is also invalid", :user_id => user.id)).not_to be_valid
+  end
+
+  it "is not valid without content" do
+    expect(Message.new(:user_id => user.id, :board_id => board.id)).not_to be_valid
+  end
+
 end
