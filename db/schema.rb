34c999f4361c2fdb0b0dd473161531488d89a681
AspenJames <aspenjames@tqca.org>
2018-06-20 14:38:26 -0700

    add description column to Board model/database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_233114) do
+ActiveRecord::Schema.define(version: 2018_06_20_213737) do
 
   create_table "boards", force: :cascade do |t|
     t.string "topic"
     t.integer "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "messages", force: :cascade do |t|
