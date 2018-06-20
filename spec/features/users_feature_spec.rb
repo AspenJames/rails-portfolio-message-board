d97a88f427befd9f9c8097bdc4c9610b012fe6f3
AspenJames <aspenjames@tqca.org>
2018-06-20 15:18:56 -0700

    add sign in and log in feature specs

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -4,8 +4,34 @@ describe "Feature Test: User Signup", :type => :feature do
 
   it "successfully signs up" do
     visit '/signup' # CHANGE ROUTE FOR USERS/NEW
+    # user_signup method is defined in login_helper.rb
     expect(current_path).to eq('/signup')
-    
+    user_signup
+    expect(current_path).to eq(root_path) # FILL IN ROOT_PATH
+  end
+
+  it "on sign up, successfully adds a session hash" do
+    visit '/signup'
+    # user_signup method is defined in login_helper.rb
+    user_signup
+    expect(page.get_rack_session_key('user_id')).to_not be_nil
+  end
+
+  it "successfully logs in a user" do
+    # create_user and user_login are defined in login_helper.rb
+    create_user
+    visit '/signin'
+    expect(page.current_path).to eq('/signin')
+    user_login
+    expect(current_path).to eq(root_path)
+  end
+
+  it "on login, successfully adds a session hash" do
+    # create_user and user_login are defined in login_helper.rb
+    create_user
+    visit '/signin'
+    user_login
+    expect(page.get_rack_session_key('user_id')).to_not be_nil
   end
 
 end
