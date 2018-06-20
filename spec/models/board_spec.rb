eb944b4c94e46ae5f8eb9966392d31870d4a345c
AspenJames <aspenjames@tqca.org>
2018-06-19 17:26:11 -0700

    add Board association specs

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -45,4 +45,30 @@ RSpec.describe Board, type: :model do
     expect(Board.new(:topic => "Invalid Data Protections")).not_to be_valid
   end
 
+  it "has many messages" do
+    expect(board.messages).to include(message_one)
+    expect(board.messages).to include(message_two)
+  end
+
+  it "has many users through messages" do
+    user_two = User.create(
+      :name => "John Doe",
+      :username => "jdoe",
+      :email => "jdoe@email.co",
+      :password => "password"
+    )
+    message_three = Message.create(
+      :content => "A wild message appears!",
+      :user_id => user_two.id,
+      :board_id => board.id
+    )
+
+    expect(board.users).to include(user)
+    expect(board.users).to include(user_two)
+  end
+
+  it "has a method to return the creator of the board" do
+    expect(board.creator).to eq(user)
+  end
+
 end
