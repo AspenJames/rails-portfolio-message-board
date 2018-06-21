b507d2ad01fcbe0ea37c4003d45d8924eb743302
AspenJames <aspenjames@tqca.org>
2018-06-20 17:44:05 -0700

    build create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(:username => params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
 
 end
