45c0005229bae7bec81a619ef034062aae79ff41
AspenJames <aspenjames@tqca.org>
2018-06-20 18:03:09 -0700

    expand signin spec to include failed signup behavior

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -15,7 +15,23 @@ describe "Feature Test: User Signup", :type => :feature do
     # user_signup method is defined in login_helper.rb
     user_signup
     # get_rack_session_key method is provided by rack_session_access gem
+    # binding.pry
     expect(page.get_rack_session_key('user_id')).to_not be_nil
+  end
+
+  it "re-renders the signup page without logging in if no username is given" do
+    visit '/signup'
+    user_signup_no_username
+    binding.pry
+    expect(current_path).to eq('/signup')
+    expect(page.get_rack_session.has_key?('user_id')).to_not be true
+  end
+
+  it "re-renders the signup page without logging in if no password is given" do
+    visit '/signup'
+    user_signup_no_password
+    expect(current_path).to eq('/signup')
+    expect(page.get_rack_session.has_key?('user_id')).to_not be true
   end
 
   it "successfully logs in a user" do
