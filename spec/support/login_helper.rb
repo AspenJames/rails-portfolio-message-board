d68ebc0b226dcad32f190ee0e6c4160f0ed02080
AspenJames <aspenjames@tqca.org>
2018-06-21 12:15:27 -0700

    define more helpers for feature tests

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -8,6 +8,25 @@ module LoginHelper
     click_button('Sign Up')
   end
 
+  def aspen_login
+    fill_in("user[username]", :with => "ajames")
+    fill_in("user[password]", :with => "password")
+    click_button("Log In")
+  end
+
+  def user_2_signup
+    fill_in("user[username]", :with => "k_law")
+    fill_in("user[email]", :with => "k_law@email.co")
+    fill_in("user[password]", :with => "password")
+    click_button('Sign Up')
+  end
+
+  def k_law_login
+    fill_in("user[username]", :with => "k_law")
+    fill_in("user[password]", :with => "password")
+    click_button("Log In")
+  end
+
   def user_signup_no_username
     fill_in("user[name]", :with => "Aspen James")
     fill_in("user[email]", :with => "aspen@email.co")
