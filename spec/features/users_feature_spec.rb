8f8f5814606c3b93e69dd3edbe9860d46b94e753
AspenJames <aspenjames@tqca.org>
2018-06-20 18:12:15 -0700

    add spec for displaying error messages

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -15,14 +15,12 @@ describe "Feature Test: User Signup", :type => :feature do
     # user_signup method is defined in login_helper.rb
     user_signup
     # get_rack_session_key method is provided by rack_session_access gem
-    # binding.pry
     expect(page.get_rack_session_key('user_id')).to_not be_nil
   end
 
   it "re-renders the signup page without logging in if no username is given" do
     visit '/signup'
     user_signup_no_username
-    binding.pry
     expect(current_path).to eq('/signup')
     expect(page.get_rack_session.has_key?('user_id')).to_not be true
   end
@@ -32,6 +30,13 @@ describe "Feature Test: User Signup", :type => :feature do
     user_signup_no_password
     expect(current_path).to eq('/signup')
     expect(page.get_rack_session.has_key?('user_id')).to_not be true
+  end
+
+  it "displays error messages when form is submitted improperly" do
+    visit '/signup'
+    user_signup_no_password
+    # Message below to account for how the HTML is returned by Capybara
+    expect(page.body).to include("Password can&#39;t be blank")
   end
 
   it "successfully logs in a user" do
