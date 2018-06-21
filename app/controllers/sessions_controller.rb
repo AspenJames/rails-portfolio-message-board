578c80f23ce7cb5f9de015615e4a80994a241424
AspenJames <aspenjames@tqca.org>
2018-06-20 18:24:38 -0700

    add flash alert error message for incorrect login credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
+      flash[:alert] = "Incorrect username and/or password"
       render :new
     end
   end
