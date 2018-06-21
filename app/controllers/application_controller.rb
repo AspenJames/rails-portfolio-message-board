a5a3be80ba21e56132ae1effeb2bc186907bae27
AspenJames <aspenjames@tqca.org>
2018-06-21 12:25:08 -0700

    define #require_login helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
    @current_user ||= User.find(session[:user_id]) if session[:user_id]
  end
 
+ def require_login
+   unless logged_in?
+     flash[:alert] = "You must be logged in to view the page you requested."
+     redirect_to login_path
+   end
+ end
+
 end
