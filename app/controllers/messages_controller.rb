94addd08f211a5459f6193b27848bc4ddaf0f5ec
AspenJames <aspenjames@tqca.org>
2018-06-22 11:39:37 -0700

    remove unnecessary hidden field

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+
+  def create
+    @board = Board.find(params[:board_id])
+    @board.messages.build(message_params)
+      if @board.save
+        redirect_to board_path(@board)
+      else
+        render 'boards#show'
+      end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :content)
+  end
+
 end
