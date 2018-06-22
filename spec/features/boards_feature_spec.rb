0f566a00aa27c16d697c3c54ddb00b44eb3465b9
AspenJames <aspenjames@tqca.org>
2018-06-21 21:05:23 -0700

    init files for Board feature specs

---
+++ spec/features/boards_feature_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../rails_helper"
+
+describe "Feature Test: Boards Index", :type => :feature do
+
+end
+
+describe "Feature Test: Board Show", :type => :feature do
+
+end
+
+describe "Feature Test: Board Edit", :type => :feature do
+  
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ RSpec.configure do |config|
 
   config.include Capybara::DSL
   config.include LoginHelper, :type => :feature
+  config.include BoardsHelper, :type => :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
