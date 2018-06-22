0ed571e29f380ff001ed238d4b2854a04cb2e977
AspenJames <aspenjames@tqca.org>
2018-06-21 21:15:34 -0700

    define helper methods for board feature specs

--- spec/support/boards_helper.rb
+++ spec/support/boards_helper.rb
@@ -1,3 +1,32 @@
 module BoardsHelper
 
+  def fill_board_form
+    # call user_signup from login_helper.rb to log in ajames
+    fill_in("board[topic]", :with => "Happy Hour")
+    click_button("Create Board")
+  end
+
+  def create_data
+    @aspen = User.create(
+      :name => "Aspen James",
+      :username => "ajames",
+      :email => "ajames@email.co",
+      :password => "password"
+    )
+    @jdoe = User.create(
+      :name => "John Doe",
+      :username => "jdoe",
+      :email => "jdoe@anon.com",
+      :password => "password"
+    )
+    @lunch_board = Board.create(
+      :topic => "Lunchtime!",
+      :created_by => @aspen.id
+    )
+    @coffee_board = Board.create(
+      :topic => "Coffee Break",
+      :created_by => @jdoe.id
+    )
+  end
+
 end
