a14760032716640196f53147b6b60b7717133d03
AspenJames <aspenjames@tqca.org>
2018-06-21 21:24:38 -0700

    add more helpers

--- spec/support/boards_helper.rb
+++ spec/support/boards_helper.rb
@@ -1,12 +1,13 @@
 module BoardsHelper
 
   def fill_board_form
-    # call user_signup from login_helper.rb to log in ajames
+    # ensure a user is logged in when using this method!
     fill_in("board[topic]", :with => "Happy Hour")
     click_button("Create Board")
   end
 
   def create_data
+    # creates two users, two boards, and three messages
     @aspen = User.create(
       :name => "Aspen James",
       :username => "ajames",
@@ -26,6 +27,21 @@ module BoardsHelper
     @coffee_board = Board.create(
       :topic => "Coffee Break",
       :created_by => @jdoe.id
+    )
+    @message1 = Message.create(
+      :content => "Message1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+      :user_id => @aspen.id,
+      :board_id => @coffee_board.id
+    )
+    @message2 = Message.create(
+      :content => "Message2 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      :user_id => @jdoe.id,
+      :board_id => @coffee_board.id
+    )
+    @message3 = Message.create(
+      :content => "Message3 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      :user_id => @aspen.id,
+      :board_id => @coffee_board.id
     )
   end
 
