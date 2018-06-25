658b84a27c2bf91dfe9c99e4e693e30a72d67e96
AspenJames <aspenjames@tqca.org>
2018-06-25 09:46:34 -0700

    add controller actions for show and edit, plus associated views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,10 +10,23 @@ class MessagesController < ApplicationController
       end
   end
 
+  def show
+    set_board_and_message
+  end
+
+  def edit
+    set_board_and_message
+  end
+
   private
 
   def message_params
     params.require(:message).permit(:user_id, :content)
+  end
+
+  def set_board_and_message
+    @board = Board.find(params[:board_id])
+    @message = Message.find(params[:id])
   end
 
 end
