292b3f31243d18c7fd0fd837f766024682cdbda6
AspenJames <aspenjames@tqca.org>
2018-06-25 10:19:35 -0700

    move time_posted from helper to model, add time_updated method

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,7 +1,2 @@
 module MessagesHelper
-
-  def time_posted
-    self.created_at.strftime("%A, %b %e %Y, %l:%m %p")
-  end
-  
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,12 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, :presence => true
+
+  def time_posted
+    self.created_at.strftime("%A, %b %e %Y, %l:%m %p")
+  end
+
+  def time_updated
+    self.updated_at.strftime("%A, %b %e %Y, %l:%m %p")
+  end
 end
