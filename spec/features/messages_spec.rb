cfae0e9d3fae7ef39cc2a8aba4b06a771e4815b5
AspenJames <aspenjames@tqca.org>
2018-06-25 09:42:58 -0700

    fix board_message_path arguments

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -8,19 +8,19 @@ describe "Feature Test: Message Show", :type => :feature do
   end
 
   it "shows an individual message" do
-    visit board_message_path(@message1)
-    expect(current_path).to eq(board_message_path(@message1))
+    visit board_message_path(@coffee_board, @message1)
+    expect(current_path).to eq(board_message_path(@coffee_board, @message1))
     expect(page).to have_content(@message1.content)
     expect(page).not_to have_content(@message2.content)
   end
 
   it "displays an edit link if the user created the message" do
-    visit board_message_path(@message1)
-    expect(page).to have_link("Edit Message", :href => edit_board_message_path(@message1))
+    visit board_message_path(@coffee_board, @message1)
+    expect(page).to have_link("Edit Message", :href => edit_board_message_path(@coffee_board, @message1))
   end
 
   it "does not display an edit link if the user did not create the message" do
-    visit board_message_path(@message2)
+    visit board_message_path(@coffee_board, @message2)
     expect(page).not_to have_link("Edit Message")
   end
 end
@@ -33,26 +33,26 @@ describe "Feature Test: Message Edit", :type => :feature do
   end
 
   it "redirects to the message show page with error message if the user did not create the message" do
-    visit edit_board_message_path(@message2)
-    expect(current_path).to eq(board_message_path(@message2))
+    visit edit_board_message_path(@coffee_board, @message2)
+    expect(current_path).to eq(board_message_path(@coffee_board, @message2))
     expect(page).to have_content("You are not authorized to edit this message")
   end
 
   it "renders an edit form if the user created the message" do
-    visit edit_board_message_path(@message1)
+    visit edit_board_message_path(@coffee_board, @message1)
     expect(page).to have_field("message[content]", :value => @message1.content)
   end
 
   it "successfully updates the message with valid data" do
-    visit edit_board_message_path(@message1)
+    visit edit_board_message_path(@coffee_board, @message1)
     fill_in("message[content]", :with => "Edited message Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
     click_button("Update Message")
-    expect(page).to eq(board_message_path(@message1))
+    expect(page).to eq(board_message_path(@coffee_board, @message1))
     expect(page).to have_content("Edited message Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
   end
 
   it "does not update with invalid data" do
-    visit edit_board_message_path(@message1)
+    visit edit_board_message_path(@coffee_board, @message1)
     fill_in("message[content]", :with => "   ")
     click_button("Update Message")
     expect(page).to have_content("Content can't be blank")
