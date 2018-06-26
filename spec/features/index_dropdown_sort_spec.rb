053c6ba87b2a48af7dc709c0014af1eb849131b5
AspenJames <aspenjames@tqca.org>
2018-06-26 12:59:55 -0700

    fix login error in before action, change select class to reflect change in form

--- spec/features/index_dropdown_sort_spec.rb
+++ spec/features/index_dropdown_sort_spec.rb
@@ -3,17 +3,17 @@ require_relative "../rails_helper"
 describe "Feature Test: Dropdown Sort", :type => :feature do
   before(:each) do
     create_data # defined in boards_helper.rb
-    visit signup_path
-    user_signup # defined in login_helper.rb
+    visit login_path
+    aspen_login # defined in login_helper.rb
     visit boards_path
   end
 
   it "renders a dropdown menu for sorting options" do
-    expect(page).to have_css("select#dropdown-menu")
+    expect(page).to have_css("select#sort")
   end
 
   it "sorts the boards alphabetically" do
-    select("Alphabetical", :from => "dropdown-menu")
+    select("Alphabetical", :from => "sort")
     click_button("Sort!")
     expect(page).to have_css("article h3:nth-child(1)", :text => "Coffee Break")
     expect(page).to have_css("article h3:nth-child(2)", :text => "Lunchtime!")
