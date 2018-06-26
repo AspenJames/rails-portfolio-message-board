8aa31328acd81b943fc781ac1826403a864455af
AspenJames <aspenjames@tqca.org>
2018-06-26 15:52:06 -0700

    add flash helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_notice
+    if flash[:notice]
+      render :partial => "application/flash_notice"
+    end
+  end
+
+  def flash_alert
+    if flash[:alert]
+      render :partial => "application/flash_alert"
+    end
+  end
 end
