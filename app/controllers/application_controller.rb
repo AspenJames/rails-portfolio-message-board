2ebe12575a1b47ca12a113045ece1644a5ce6439
AspenJames <aspenjames@tqca.org>
2018-08-03 11:37:26 -0700

    fix whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,17 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-   !!current_user
+    !!current_user
   end
 
- def current_user
-   @current_user ||= User.find(session[:user_id]) if session[:user_id]
- end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
- def require_login
-   unless logged_in?
-     flash[:alert] = "You must be logged in to view the page you requested."
-     redirect_to root_path
-   end
- end
-
+  def require_login
+    unless logged_in?
+      flash[:alert] = "You must be logged in to view the page you requested."
+      redirect_to root_path
+    end
+  end
 end
