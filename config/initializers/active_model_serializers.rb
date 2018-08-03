94f2340393e3d27c6963cb48d08f6000ad8e854d
AspenJames <aspenjames@tqca.org>
2018-08-03 12:00:05 -0700

    edit config

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,1 +1,5 @@
-ActiveModelSerializers.config.adapter = :json_api # Default: `:attributes`
+ActiveModelSerializers.config.tap do |c|
+  c.adapter = :json_api # Default: `:attributes`
+  c.jsonapi_include_toplevel_object = true
+  c.jsonapi_version = "1.0"
+end
