02cb7493976fa14c88cba855f614d636bf8bb2ee
AspenJames <aspenjames@tqca.org>
2018-08-07 10:46:03 -0700

    render board data with jquery

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -1,3 +1,15 @@
 $(function(){
-  console.log(params);
-})
+  let id = $("#boardTopic").data("id");
+  $.getJSON(`/boards/${id}`, function(data){
+    // assign creator name if exists, default to username
+    let creator = data["board"]["creator"]["name"] || data["board"]["creator"]["username"];
+    // update DOM elements
+    $("#boardTopic").text(data["board"]["topic"]);
+    $("#boardDescription").text(data["board"]["description"]);
+    $("#boardCreator").text(creator);
+    // These two elements only exist on the DOM if the current user is the
+    // creator of the board.
+    $("#editBoard").html(`<a href="/boards/${id}/edit">Edit Board</a>`)
+    $("#deleteBoard").html(`<a href="/boards/${id}/delete">Delete Board</a>`)
+  });
+});
