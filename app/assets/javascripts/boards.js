c1fecc5f4129df3d67eaad465c8f87766471b70c
AspenJames <aspenjames@tqca.org>
2018-08-07 11:41:46 -0700

    submit new message form with jquery

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -3,7 +3,7 @@ $(function(){
   let id = $("#boardTopic").data("id");
   // compile handlebars template for messages
   let template = Handlebars.compile($("#messageTemplate").html());
-
+  // request JSON data for board
   $.getJSON(`/boards/${id}`, function(data){
     // assign creator name if exists, default to username
     let creator = data["board"]["creator"]["name"] || data["board"]["creator"]["username"];
@@ -26,6 +26,27 @@ $(function(){
       };
       //append to messagesDiv
       $("#messagesDiv")[0].innerHTML += template(data);
+    });
+  });
+  // grab form submission event, prevent page reload
+  $("#new_message").on("submit", function(event){
+    event.preventDefault();
+    $.post(`/boards/${id}/messages`, $(this).serialize(), function(response){
+      //build message data JSON
+      let data = {
+        "messageContent": response["message"]["content"],
+        "boardID": id,
+        "messageID": response["message"]["id"],
+        "userID": response["message"]["user"]["id"],
+        "username": response["message"]["user"]["username"]
+      };
+      //append to messagesDiv
+      $("#messagesDiv")[0].innerHTML += template(data);
+
+      //clear form data, re-enable submit button, re-focus input field
+      $("textarea").val("");
+      $("input[type='submit']")[0].disabled = false;
+      $("textarea").focus();
     });
   });
 });
