db2a079d48c864f2ab974a9efa75edeb7b1c8133
AspenJames <aspenjames@tqca.org>
2018-08-06 18:50:22 -0700

    change int limit to accomodate Twitter login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_161853) do
+ActiveRecord::Schema.define(version: 2018_08_07_014723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2018_06_21_161853) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "uid"
+    t.bigint "uid"
     t.string "provider"
   end
 
