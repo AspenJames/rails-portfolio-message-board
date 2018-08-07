e2fe6919d4daff636ac8143f3f1b61083978d3ce
AspenJames <aspenjames@tqca.org>
2018-08-07 11:10:32 -0700

    append messages to jquery view using handlebars

--- app/assets/javascripts/boards.js
+++ app/assets/javascripts/boards.js
@@ -1,5 +1,9 @@
 $(function(){
+  // grab ID of current board requested
   let id = $("#boardTopic").data("id");
+  // compile handlebars template for messages
+  let template = Handlebars.compile($("#messageTemplate").html());
+
   $.getJSON(`/boards/${id}`, function(data){
     // assign creator name if exists, default to username
     let creator = data["board"]["creator"]["name"] || data["board"]["creator"]["username"];
@@ -11,5 +15,17 @@ $(function(){
     // creator of the board.
     $("#editBoard").html(`<a href="/boards/${id}/edit">Edit Board</a>`)
     $("#deleteBoard").html(`<a href="/boards/${id}/delete">Delete Board</a>`)
+    data["board"]["messages"].forEach(function(message){
+      //build message data JSON
+      let data = {
+        "messageContent": message["content"],
+        "boardID": id,
+        "messageID": message["id"],
+        "userID": message["user"]["id"],
+        "username": message["user"]["username"]
+      };
+      //append to messagesDiv
+      $("#messagesDiv")[0].innerHTML += template(data);
+    });
   });
 });
