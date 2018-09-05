d7382e570f4a86b1dfd42227c29c3a693deb7e29
Aspen James <aspenjames@tqca.org>
2018-09-04 17:58:26 -0700

    update database connection + seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 User.create(
+  #id:1
   :name => "Aspen James",
   :username => "ajames",
   :email => "ajames@email.co",
@@ -6,6 +7,7 @@ User.create(
 )
 
 User.create(
+  #id:2
   :name => "Average Joe",
   :username => "avjoe",
   :email => "avjoe@email.co",
@@ -13,25 +15,36 @@ User.create(
 )
 
 User.create(
+  #id:3
   :username => "john_doe",
   :email => "jdoe@anon.gov",
   :password => "password"
 )
 
+User.create(
+  #id:4
+  :username => "guest",
+  :email => "guest@example.com",
+  :password => "demo"
+)
+
 Board.create(
+  #id:1
   :topic => "Lunch Hour",
   :description => "Best spots to get lunch around the office",
   :created_by => 2
 )
 
 Board.create(
+  #id:2
   :topic => "Coffee Break",
   :created_by => 1
 )
 
 Board.create(
-  :topic => "These are all break related",
-  :description => "Does anyone do any work around here?",
+  #id:3
+  :topic => "Switch to Linux",
+  :description => "Advice and help on switching your workstation to a Linux OS",
   :created_by => 3
 )
 
@@ -68,5 +81,17 @@ Message.create(
 Message.create(
   :content => "6. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   :user_id => 3,
+  :board_id => 1
+)
+
+Message.create(
+  :content => "Guest example message Maecenas varius dui nunc, vel molestie ipsum efficitur pretium. Nullam vestibulum, mi non mollis porta, dui felis convallis nisi, a lacinia metus turpis at eros. Etiam nec velit dictum metus porta vehicula. Sed vel fermentum mi, ut vestibulum eros. Nam porta nulla nibh, ut lacinia erat venenatis quis. Integer condimentum rhoncus arcu eget auctor. Donec nec ultricies nunc. In egestas elit at libero faucibus pretium.",
+  :user_id => 4,
+  :board_id => 3
+)
+
+Message.create(
+  :content => "Guest example message Pellentesque aliquam dapibus odio id ultrices. Praesent efficitur ante et maximus lobortis. Pellentesque molestie eros leo, eu volutpat magna rhoncus quis. Aliquam vestibulum lorem fringilla posuere ornare. Integer fringilla eget diam sed eleifend. Etiam mollis, ex a convallis condimentum, leo orci posuere magna, vel aliquam nunc massa malesuada turpis. Vestibulum at condimentum ipsum. Nullam ultricies est et neque mollis tincidunt. Vestibulum accumsan lobortis tortor non vestibulum. Suspendisse condimentum gravida lacus nec consectetur. Vivamus tempor nisi in leo ullamcorper porta.",
+  :user_id => 4,
   :board_id => 1
 )
